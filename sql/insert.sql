13890c83482198f7b9c3362dd998ca933ca69553
RubenTorres <rtorresgutierrez.guadalupe@alumnado.fundacionloyola.net>
2021-12-10 13:57:26 +0100

    sql

---
+++ sql/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO miniJuego(nombre, url)
+VALUES 	('Tabla Periodica', '/minijuegos/tablaPeriodica'),
+	('Flashcards', '/minijuegos/flashcards'),
+	('Ligas', '/minijuegos/ligas'),
+	('Reciclaje', '/minijuegos/reciclaje'),
+	('Crazy Balls', '/minijuegos/crazyBalls')
---
+++ sql/script.sql
@@ -0,0 +1,34 @@
+-- BASE DE DATOS
+-- ELIMINAR BASE DE DATOS SI EXISTE
+DROP DATABASE IF EXISTS RegistroMinijuego;
+-- CREAR BASE DE DATOS
+CREATE DATABASE RegistroMinijuego DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
+-- SELECCIONAR LA BASE DE DATOS
+USE RegistroMinijuego;
+
+-- TABLAS
+-- Tabla usuario
+CREATE TABLE usuario(
+    idUsario smallint UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre varchar(15) NOT NULL,
+    apellidos varchar(30) NOT NULL,
+    nombreUsuario varchar(15) NOT NULL,
+    correo varchar(100) NOT NULL UNIQUE,
+    password varchar(20) NOT NULL
+);
+
+-- Tabla Minijuego
+CREATE TABLE miniJuego(
+    idMinijuego tinyint UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre varchar(20) NOT NULL,
+    url varchar(100) NOT NULL
+);
+
+-- Tabla Preferencia
+CREATE TABLE preferencia(
+    idUsario smallint UNSIGNED NOT NULL,
+    idMinijuego tinyint UNSIGNED NOT NULL,
+    CONSTRAINT FK_idUsuario FOREIGN KEY (idUsario) REFERENCES usuario(idUsario),
+    CONSTRAINT FK_idMinijuego FOREIGN KEY (idMinijuego) REFERENCES miniJuego(idMinijuego),
+    CONSTRAINT PK_preferencia PRIMARY KEY (idUsario, idMinijuego)
+);
